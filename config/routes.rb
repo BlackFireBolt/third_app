f0675fda9276d277e76d088e34392e1442f09045
Andrey Boltikov <boltward@gmail.com>
2017-05-16 13:27:15 +0300

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  namespace :users do
+    get 'omniauth_callbacks/facebook'
+  end
+
+  namespace :users do
+    get 'omniauth_callbacks/vkontakte'
+  end
+
+  namespace :users do
+    get 'omniauth_callbacks/twitter'
+  end
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/callbacks" }
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
